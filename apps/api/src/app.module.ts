9bdec4efffe6faa45a82e6157a731a1012a6b58d
ahmedgamer4 <ahmedalseidy4@gmail.com>
2024-12-03 17:12:03 +0200

    fix typo

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { UsersService } from '@ebay-clone/nestjs-libs';
 import { AuthController } from './routes/auth.controller';
 import { AuthService } from './services/auth/auth.service';
@@ -10,13 +10,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './services/auth/strategies/jwt.strategy';
 import refreshConfig from './services/auth/config/refresh.config';
 import { RefreshStrategy } from './services/auth/strategies/refresh-jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './services/auth/guards/jwt-auth/jwt-auth.guard';
 
-const providers = [
+const providers: Provider[] = [
   UsersService,
   AuthService,
   LocalStrategy,
   JwtStrategy,
   RefreshStrategy,
+  {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },
 ];
 const controllers = [AuthController];
 
