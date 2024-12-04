f9829172f79995b3d4a1b90edb616e65b43486c0
ahmedgamer4 <ahmedalseidy4@gmail.com>
2024-12-04 20:59:50 +0200

    Modify: login and register page

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -4,14 +4,19 @@ import { SigninForm } from "./signin-form";
 
 export default function SignupPage() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center ">
-      <h1 className="text-center text-2xl font-bold mb-4">Sign In Page</h1>
-      <SigninForm />
-      <div className="flex justify-between text-sm">
-        <p>Don't have an account?</p>
-        <Link className="underline" href={"/auth/signup"}>
-          Sign Un
-        </Link>
+    <div className="bg-white p-6 rounded-lg border border-1 shadow-sm w-96 flex flex-col justify-center items-center ">
+      <div className="w-full">
+        <h1 className="text-2xl font-bold mb-1">Login</h1>
+        <p className="text-sm mb-4 text-gray-700">
+          Enter your email below to login to your account
+        </p>
+        <SigninForm />
+        <div className="flex justify-between text-sm mt-3">
+          <p>Don't have an account?</p>
+          <Link className="underline" href={"/auth/signup"}>
+            Sign Up
+          </Link>
+        </div>
       </div>
     </div>
   );
--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -4,14 +4,27 @@ import { SignupForm } from "./signup-form";
 
 export default function SignupPage() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center ">
-      <h1 className="text-center text-2xl font-bold mb-4">Sign In Page</h1>
-      <SignupForm />
-      <div className="flex justify-between text-sm">
-        <p>Already have an account?</p>
-        <Link className="underline" href={"/auth/signin"}>
-          Sign In
-        </Link>
+    <div className="bg-white flex items-center w-full">
+      <div className="bg-foreground w-1/2 min-h-screen flex flex-col justify-between p-12 text-white">
+        <h3 className="text-3xl">Ubay</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          repellat nostrum tenetur maxime earum possimus placeat consequatur
+          dolorum voluptate explicabo? Unde deleniti amet tempore alias nobis
+          similique cupiditate sint error.
+        </p>
+      </div>
+      <div className="bg-white w-1/2 flex justify-center">
+        <div className="w-80">
+          <h1 className="text-center text-2xl font-bold mb-4">Sign In Page</h1>
+          <SignupForm />
+          <div className="flex justify-between text-sm">
+            <p>Already have an account?</p>
+            <Link className="underline" href={"/auth/signin"}>
+              Sign In
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
